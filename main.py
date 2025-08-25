eeda9ab1a8c7bffc503eb4f1cc8278868612398a
FishEatRice <140298215+FishEatRice@users.noreply.github.com>
2025-08-25 18:59:31 +0800

    Clearning Old File

--- main.py
+++ main.py
@@ -1,13 +1,1 @@
-import streamlit as st
-from function.data_loader import load_data
-
-# 初始化 session_state
-if 'recommended_count' not in st.session_state:
-    st.session_state.recommended_count = 6
-if 'filter_hentai' not in st.session_state:
-    st.session_state.filter_hentai = True
-
-# 加载数据
-df = load_data()
-
-st.Page("homepage.py", title="test 1"),
+AS
