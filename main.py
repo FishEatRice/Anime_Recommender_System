c2afeb3d9d7e42771ffc3c93e2ef7c784e67f896
FishEatRice <140298215+FishEatRice@users.noreply.github.com>
2025-08-31 01:38:37 +0800

    BackUPPPPP

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import streamlit as st
 
 pages = [
-    st.Page("pages/anime_recommender.py", title="Anime Recommender"),
+    st.Page("pages/anime_title_recommender.py", title="Anime Title Recommender"),
     st.Page("pages/settings_page.py", title="Settings")
 ]
 
 pg = st.navigation(pages, position="top")
 pg.run()
 
-st.Page("pages/anime_recommender.py", title="Anime Recommender")
+st.Page("pages/anime_title_recommender.py", title="Anime Recommender")
