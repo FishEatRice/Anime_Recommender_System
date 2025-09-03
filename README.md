39aaae2938cea683ebe05d3989dbf177eebee6fa
FishEatRice <140298215+FishEatRice@users.noreply.github.com>
2025-09-03 20:31:43 +0800

    backup

--- README.md
+++ README.md
@@ -5,8 +5,8 @@ Google Drive Center:
 https://drive.google.com/drive/folders/1MJIW1NkbF7bHvTI9YXpyeFMPB19cgayj?usp=drive_link
 
 Anime_data.csv from: 
-https://www.kaggle.com/datasets/vishalmane109/anime-recommendations-database/data
 https://www.kaggle.com/datasets/trijalvijaykhade/all-anime-details?resource=download&select=anime_400_cleaned.csv
+https://www.kaggle.com/datasets/marlesson/myanimelist-dataset-animes-profiles-reviews/data?select=reviews.csv
 
 # Plan/Mission:
 - Pandas 
--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import streamlit as st
 
-pages = [
-    st.Page("pages/anime_title_recommender_page.py", title="Anime Title Recommender"),
-    st.Page("pages/anime_genre_recommender_page.py", title="Anime Genre Recommender"),
-    st.Page("pages/settings_page.py", title="Settings")
-]
+pages = {
+    "🏠Main": [st.Page("pages/main_page.py", title="Main Page")],
+    "🕵️Recommender System": [
+        st.Page("pages/anime_title_recommender_page.py", title="Anime Title"),
+        st.Page("pages/anime_genre_recommender_page.py", title="Anime Genre"),
+    ],
+    "⚙️Settings": [st.Page("pages/settings_page.py", title="Settings Page")],
+}
 
-pg = st.navigation(pages, position="top")
+pg = st.navigation(pages)
 pg.run()
 
-st.Page("pages/anime_title_recommender_page.py", title="Anime Recommender")
+
+st.Page("pages/main_page.py", title="Main Page")
