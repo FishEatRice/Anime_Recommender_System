15f0fdc4eaf977bac079c97c16f39dfa879acd1f
FishEatRice <140298215+FishEatRice@users.noreply.github.com>
2025-09-08 18:42:48 +0800

    update

--- run_streamlit.bat
+++ run_streamlit.bat
@@ -4,7 +4,6 @@ echo Install Streamlit...
 pip install streamlit
 pip install scikit-learn
 pip install fuzzywuzzy
-pip install python-Levenshtein
 pip install streamlit-scroll-to-top
 
 echo Start Application
