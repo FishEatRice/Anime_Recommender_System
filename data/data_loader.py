91b8cbad9717cf8d7d7266a838274921db443f17
FishEatRice <140298215+FishEatRice@users.noreply.github.com>
2025-09-11 18:30:33 +0800

    Update for low storage

--- data/data_loader.py
+++ data/data_loader.py
@@ -1,17 +1,19 @@
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
+import streamlit as st
 
+@st.cache_data
 def load_data(animes_path="data/animes.csv", reviews_path="data/reviews.csv"):
     df_animes = pd.read_csv(animes_path, encoding="utf-8")
     df_reviews = pd.read_csv(reviews_path, encoding="ISO-8859-1")
 
-    df_animes = df_animes[~df_animes['genre'].str.contains('Hentai', case=False, na=False)]
     df_animes = df_animes[['uid', 'title', 'genre', 'synopsis', 'score', 'link']].dropna()
     df_reviews = df_reviews[['profile', 'anime_uid', 'rating']].dropna()
 
     return df_animes, df_reviews
     
+@st.cache_data
 def get_anime_picture(url):
     try:
         response = requests.get(url, headers={"User-Agent": "Mozilla/5.0"})
